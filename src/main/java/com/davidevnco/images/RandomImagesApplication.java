c8fc3c8d4506b5d01adaaeb7094247606e245628
David <david.pineda@campsited.com>
2022-05-10 19:52:39 +0100

    initial

---
+++ src/main/java/com/davidevnco/images/RandomImagesApplication.java
@@ -0,0 +1,13 @@
+package com.davidevnco.images;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomImagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomImagesApplication.class, args);
+	}
+
+}
